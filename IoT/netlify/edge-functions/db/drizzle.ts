433f4eb65a7093a51c972a292b14c323ea9eb101
multi05369 <tentingsanook@gmail.com>
2025-07-23 10:01:14 +0700

    Revert "fix: fix bug for deploy"
    This reverts commit ee340065d4c6242b58ba482ae0ebb3c09b34e09a.

--- IoT/netlify/edge-functions/db/drizzle.ts
+++ IoT/netlify/edge-functions/db/drizzle.ts
@@ -3,7 +3,7 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema.ts'
 
-const sql = neon(Deno.env.get('POSTGRES_URL'))
+const sql = neon(Deno.env.get('POSTGRES_URL')!)
 const db = drizzle(sql, { schema })
 
 export default db
